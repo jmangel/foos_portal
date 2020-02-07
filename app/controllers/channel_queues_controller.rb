0abdc4f3ae04702b0ca6af92e8ab9d1d7b369f98
John Mangel <john.mangel@appfolio.com>
2020-02-07 15:03:33 -0800

    jm - verify queues requests come from slack

--- app/controllers/channel_queues_controller.rb
+++ app/controllers/channel_queues_controller.rb
@@ -2,7 +2,24 @@ class ChannelQueuesController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
+    return render json: { text: 'Slack signature invalid' } unless request_verified?
+
     response = ChannelQueues::ResponseRetriever.retrieve(params[:text], params[:channel_id], params[:channel_name], params[:user_id], params[:user_name])
     render json: response
+  end
+
+  private
+
+  def request_verified?
+    timestamp = request.headers['X-Slack-Request-Timestamp']
+
+    if Time.at(timestamp.to_i) < 5.minutes.ago
+      return false # expired
+    end
+
+    basestring = "v0:#{timestamp}:#{request.body.read}"
+    expected_signature = "v0=#{OpenSSL::HMAC.hexdigest("SHA256", ENV.fetch('SLACK_SIGNING_SECRET'), basestring)}"
+
+    expected_signature == request.headers['X-Slack-Signature']
   end
 end
--- test/controllers/channel_queues_controller_test.rb
+++ test/controllers/channel_queues_controller_test.rb
@@ -1,13 +1,60 @@
 require 'test_helper'
 
 class ChannelQueuesControllerTest < ActionController::TestCase
+  setup do
+    ENV['SLACK_SIGNING_SECRET'] = 'ade6ca762ade4db0e7d31484cd616b9c'
+    @timestamp = '1531221943'
+  end
+
   def test_index
+    Time.zone.stubs(:now).returns(Time.at(@timestamp.to_i) + 3.minutes)
+
+    set_headers
+
     expected_response_body = { text: 'response body text' }
     ChannelQueues::ResponseRetriever.expects(:retrieve).with('list', 'C123', 'my-channel', 'U123', 'my.user').returns(expected_response_body)
 
     get :index, { text: 'list', token: '12345', channel_id: 'C123', channel_name: 'my-channel', user_id: 'U123', user_name: 'my.user' }
+
     assert_response :ok
     body = JSON.parse(response.body).symbolize_keys
     assert_equal expected_response_body, body
+  end
+
+  def test_index__invalid_signing_secret
+    Time.zone.stubs(:now).returns(Time.at(@timestamp.to_i) + 3.minutes)
+
+    set_headers(signing_secret: 'invalid')
+
+    ChannelQueues::ResponseRetriever.expects(:retrieve).never
+
+    get :index, { text: 'list', token: '12345', channel_id: 'C123', channel_name: 'my-channel', user_id: 'U123', user_name: 'my.user' }
+
+    assert_response :ok
+    body = JSON.parse(response.body).symbolize_keys
+    assert_equal({ text: 'Slack signature invalid' }, body)
+  end
+
+  def test_index__old_timestamp
+    Time.zone.stubs(:now).returns(Time.at(@timestamp.to_i) + 6.minutes)
+
+    set_headers
+
+    ChannelQueues::ResponseRetriever.expects(:retrieve).never
+
+    get :index, { text: 'list', token: '12345', channel_id: 'C123', channel_name: 'my-channel', user_id: 'U123', user_name: 'my.user' }
+
+    assert_response :ok
+    body = JSON.parse(response.body).symbolize_keys
+    assert_equal({ text: 'Slack signature invalid' }, body)
+  end
+
+  private
+
+  def set_headers(signing_secret: ENV['SLACK_SIGNING_SECRET'])
+    request.headers['X-Slack-Request-Timestamp'] = @timestamp
+
+    basestring = "v0:#{@timestamp}:"
+    request.headers['X-Slack-Signature'] = "v0=#{OpenSSL::HMAC.hexdigest("SHA256", signing_secret, basestring)}"
   end
 end
